159de3e7a50ae11f0cc8bd9111afd976bad09df4
Archimedes Trajano <developer@trajano.net>
2014-01-01 20:40:09 -0500

    added javadoc

--- app-web/src/main/java/net/trajano/app/Bean.java
+++ app-web/src/main/java/net/trajano/app/Bean.java
@@ -16,18 +16,40 @@ public class Bean {
      */
     private String message;
 
+    /**
+     * Date. Does a copy.
+     * 
+     * @return date.
+     */
     public Date getDate() {
         return new Date(date.getTime());
     }
 
+    /**
+     * The message.
+     * 
+     * @return message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the date. Makes a copy of the input.
+     * 
+     * @param date
+     *            date.
+     */
     public void setDate(final Date date) {
         this.date = new Date(date.getTime());
     }
 
+    /**
+     * Sets the message.
+     * 
+     * @param message
+     *            message
+     */
     public void setMessage(final String message) {
         this.message = message;
     }
