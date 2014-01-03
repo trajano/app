18f22bf7c4cdba13e4cc6ad78cd72f3f3c7c60d9
Archimedes Trajano <developer@trajano.net>
2014-01-03 03:38:29 -0500

    Added javadoc

--- app-web/src/main/java/net/trajano/app/EchoEndpoint.java
+++ app-web/src/main/java/net/trajano/app/EchoEndpoint.java
@@ -6,8 +6,22 @@ import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+/**
+ * Web socket endpoint.
+ * 
+ * @author Archimedes
+ */
 @ServerEndpoint("/echo")
 public class EchoEndpoint {
+    /**
+     * Sends back the original message.
+     * 
+     * @param session
+     *            web socket session
+     * @param message
+     *            the message
+     * @throws IOException
+     */
     @OnMessage
     public void onMessage(final Session session, final String message)
             throws IOException {
