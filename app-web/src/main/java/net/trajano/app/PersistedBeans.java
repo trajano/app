050e8ac49ca27b2db1bca974ee94d7a4ff96207d
Archimedes Trajano <archie@trajano.net>
2014-01-10 14:47:14 -0500

    Added integration test with EclipseLink and Derby for JPA.

--- app-web/src/main/java/net/trajano/app/PersistedBeans.java
+++ app-web/src/main/java/net/trajano/app/PersistedBeans.java
@@ -8,52 +8,62 @@ import javax.persistence.PersistenceContext;
 /**
  * SLSB to manage persisted classes. This would be the table model.
  * 
- * @author Archimedes
+ * @author Archimedes Trajano
  */
 @Stateless
 @BusinessMethod
 public class PersistedBeans {
-    /**
-     * Entity manager.
-     */
-    @PersistenceContext
-    private EntityManager em;
+	/**
+	 * Entity manager.
+	 */
+	@PersistenceContext
+	private EntityManager em;
 
-    /**
-     * Gets a specific record.
-     * 
-     * @param id
-     *            primary key
-     * @return persisted bean
-     */
-    public PersistedBean get(final long id) {
-        return em.find(PersistedBean.class, id);
-    }
+	/**
+	 * Gets a specific record.
+	 * 
+	 * @param id
+	 *            primary key
+	 * @return persisted bean
+	 */
+	public PersistedBean get(final long id) {
+		return em.find(PersistedBean.class, id);
+	}
 
-    /**
-     * Gets the latest record.
-     * 
-     * @return latest record.
-     */
-    public PersistedBean getLatest() {
-        final PersistedBean bean = em
-                .createQuery(
-                        "select p from PersistedBean p order by p.someTimestamp desc, p.id desc",
-                        PersistedBean.class).setFlushMode(FlushModeType.AUTO)
-                .getResultList().get(0);
-        return bean;
-    }
+	/**
+	 * Gets the latest record.
+	 * 
+	 * @return latest record.
+	 */
+	public PersistedBean getLatest() {
+		final PersistedBean bean = em
+				.createQuery(
+						"select p from PersistedBean p order by p.someTimestamp desc, p.id desc",
+						PersistedBean.class).setFlushMode(FlushModeType.AUTO)
+				.getResultList().get(0);
+		return bean;
+	}
 
-    /**
-     * Save, flush and refresh a persisted bean. The flush and refreshing is
-     * needed to ensure that the temporal data contains the proper values.
-     * 
-     * @param bean
-     *            bean to persist.
-     */
-    public void save(final PersistedBean bean) {
-        em.persist(bean);
-        em.flush();
-        em.refresh(bean);
-    }
+	/**
+	 * Save, flush and refresh a persisted bean. The flush and refreshing is
+	 * needed to ensure that the temporal data contains the proper values.
+	 * 
+	 * @param bean
+	 *            bean to persist.
+	 */
+	public void save(final PersistedBean bean) {
+		em.persist(bean);
+		em.flush();
+		em.refresh(bean);
+	}
+
+	/**
+	 * Injects the entity manager for testing.
+	 * 
+	 * @param em
+	 *            entity manager.
+	 */
+	public void setEntityManager(EntityManager em) {
+		this.em = em;
+	}
 }
