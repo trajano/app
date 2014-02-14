5c77caae62c7c4743be1275681ee11e8cc15c0b3
Archimedes Trajano <developer@trajano.net>
2014-02-14 07:46:34 -0500

    Changed to throw a JMSRuntimeException

--- app-web/src/main/java/net/trajano/app/JpaAdder.java
+++ app-web/src/main/java/net/trajano/app/JpaAdder.java
@@ -4,6 +4,7 @@ import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
+import javax.jms.JMSRuntimeException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
@@ -11,30 +12,29 @@ import javax.jms.TextMessage;
 /**
  * Adds a new record to the database when there is a new message.
  * 
- * @author Archimedes
+ * @author Archimedes Trajano
  */
 @MessageDriven(activationConfig = {
-        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyQueue"),
-        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") })
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyQueue"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") })
 public class JpaAdder implements MessageListener {
 
-    /**
-     * Injected SLSB.
-     */
-    @EJB
-    private PersistedBeans persistedBeans;
+	/**
+	 * Injected SLSB.
+	 */
+	@EJB
+	private PersistedBeans persistedBeans;
 
-    @Override
-    public void onMessage(final Message message) {
+	@Override
+	public void onMessage(final Message message) {
 
-        final PersistedBean bean = new PersistedBean();
-        try {
-            bean.setMessage(((TextMessage) message).getText());
-        } catch (final JMSException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        persistedBeans.save(bean);
-    }
+		final PersistedBean bean = new PersistedBean();
+		try {
+			bean.setMessage(((TextMessage) message).getText());
+		} catch (final JMSException e) {
+			throw new JMSRuntimeException(e.getMessage(), e.getErrorCode(), e);
+		}
+		persistedBeans.save(bean);
+	}
 
 }
