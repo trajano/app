a016079bca644b8911e84870faac33e30ca2d25b
Archimedes Trajano <arch@trajano.net>
2014-07-01 23:57:59 -0400

    Relocated the security roles to the EAR level
    That's where it really should be

--- app-web/src/main/java/net/trajano/app/WebSocketSessions.java
+++ app-web/src/main/java/net/trajano/app/WebSocketSessions.java
@@ -7,19 +7,45 @@ import java.util.Map;
 import javax.ejb.Singleton;
 import javax.websocket.Session;
 
+/**
+ * Manages web socket sessions. This is a singleton as it is storing
+ * non-serializable data namely {@link Session}.
+ *
+ * @author Archimedes Trajano
+ *
+ */
 @Singleton
 public class WebSocketSessions {
+    /**
+     * Map of sessions.
+     */
     private final Map<String, Session> map = new HashMap<>();
 
+    /**
+     * Gets a collection of all sessions.
+     *
+     * @return all sessions
+     */
     public Collection<Session> getAllSessions() {
         return map.values();
     }
 
+    /**
+     * Puts a session in the collection.
+     *
+     * @param session
+     *            session
+     */
     public void put(final Session session) {
         map.put(session.getId(), session);
-        System.out.println("put " + session);
     }
 
+    /**
+     * Remove session from the collection.
+     *
+     * @param session
+     *            session
+     */
     public void remove(final Session session) {
         map.remove(session.getId());
     }
